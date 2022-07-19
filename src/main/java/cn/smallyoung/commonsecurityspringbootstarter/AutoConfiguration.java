78cd803ca7f4dc290fdd34f4cdeff215f300b606
yjc <504015995@qq.com>
2022-07-19 16:10:26 +0800

    fix 某些包为注入问题

--- src/main/java/cn/smallyoung/commonsecurityspringbootstarter/AutoConfiguration.java
+++ src/main/java/cn/smallyoung/commonsecurityspringbootstarter/AutoConfiguration.java
@@ -2,6 +2,7 @@ package cn.smallyoung.commonsecurityspringbootstarter;
 
 import cn.smallyoung.commonsecurityspringbootstarter.config.LocalDateTimeSerializerConfig;
 import cn.smallyoung.commonsecurityspringbootstarter.controller.LoggerController;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Import;
  * @date 2022/4/8
  */
 @Configuration
+@ComponentScan("cn.smallyoung.commonsecurityspringbootstarter.component")
 @Import({
         BaseRepositoryAutoConfiguration.class,
         JwtAutoConfiguration.class,
